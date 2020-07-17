4658564289f248ebe756fb0fb647a1fbb7b14291
Kian <Ksadeghi@sas.upenn.edu>
2020-07-17 17:47:44 -0400

    finds all possible differences and minimum value

--- stock-picker.rb
+++ stock-picker.rb
@@ -12,6 +12,37 @@ def str_to_int_arr(array)
     end
   end
 end
+ 
+def maximum(values) 
+  maximum = 0
+  days = nil
+  values.each do |key, value|      
+    if key > maximum 
+      maximum = key
+      days = value
+    end
+  end
+  if days != nil && maximum != 0
+  return "Day #{days[0]} you buy and day #{days[1]} you sell for a profit of $#{maximum}"
+  else 
+  "Do not buy on any of these days unless you want to make no or even lose money"
+  end
+end
+
+def all_diff(stock_values)
+  diff = Hash.new(0)
+  for i in 0..stock_values.length - 2
+    for j in i + 1..stock_values.length - 1
+      diff[stock_values[j] - stock_values[i]] = [i,j]
+    end
+  end
+  return diff
+end
+
+
+
+
+
 
 stock_prices = Array.new(0)
 puts "Hello! Please enter the space delimited stock prices you would like us to analyze"
@@ -19,5 +50,8 @@ int_inps = gets.chomp
 str_ver = int_inps.split(" ")
 stock_prices = str_to_int_arr(str_ver)
 if stock_prices != nil 
-puts "Great. Here are the days you should buy and sell:"
+  all_subtract = all_diff(stock_prices)
+  if maximum(all_subtract) != nil
+    p maximum(all_subtract)
+  end
 end
